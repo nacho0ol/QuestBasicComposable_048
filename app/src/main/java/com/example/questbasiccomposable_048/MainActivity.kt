04d512163f61d570941fe94cef6faa31d740827e
nachool <nachma.olivia1394@gmail.com>
2025-10-17 16:16:52 +0700

    memanggil TataletakBoxColumnRow pada class MainActivity

--- app/src/main/java/com/example/questbasiccomposable_048/MainActivity.kt
+++ app/src/main/java/com/example/questbasiccomposable_048/MainActivity.kt
@@ -20,8 +20,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             QuestBasicComposable_048Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    TataletakBoxColumnRow(
+
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
