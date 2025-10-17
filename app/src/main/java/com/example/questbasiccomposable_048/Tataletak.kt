9ab5acc2c694939b2cef62340ce5c082df94b32b
nachool <nachma.olivia1394@gmail.com>
2025-10-17 16:23:48 +0700

    Merubah text pada function tataletakColumn lagi

--- app/src/main/java/com/example/questbasiccomposable_048/Tataletak.kt
+++ app/src/main/java/com/example/questbasiccomposable_048/Tataletak.kt
@@ -67,9 +67,9 @@ fun TataletakColumnRow(modifier: Modifier) {
         // baris1
         Row(modifier = modifier.fillMaxWidth(),
             horizontalArrangement = Arrangement.SpaceEvenly) {
-            Text(text = "Komponen1Baris ke-1")
-            Text(text = "Komponen2Baris ke-1")
-            Text(text = "Komponen3Baris ke-1")
+            Text(text = "Komponen1 Baris ke-1")
+            Text(text = "Komponen2 Baris ke-1")
+            Text(text = "Komponen3 Baris ke-1")
         }
         //baris2
         Row(modifier = modifier.fillMaxWidth(),
